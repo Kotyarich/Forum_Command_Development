fca6fedb1c9c5327c68f68a8ff7253f1c234479c
Kotyarich <ndkotov@gmail.com>
2021-04-03 02:38:27 +0300

    [#12] Rename task property

--- task_aggregator/aggregator/services/Task.py
+++ task_aggregator/aggregator/services/Task.py
@@ -2,12 +2,12 @@ from datetime import datetime
 
 
 class Task:
-    def __init__(self, service: str, link: str, description: str, label: str,
+    def __init__(self, service: str, link: str, description: str, labels: str,
                  priority: str, deadline: datetime, difficulty: str):
         self.service = service
         self.link = link
         self.description = description
-        self.label = label
+        self.labels = labels
         self.priority = priority
         self.deadline = deadline
         self.difficulty = difficulty
