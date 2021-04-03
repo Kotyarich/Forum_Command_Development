5142927b0c12c452fa0334ea1ff2656d9170989f
Kotyarich <ndkotov@gmail.com>
2021-04-04 00:01:18 +0300

    [#12] Change task labels type hint

--- task_aggregator/aggregator/services/Task.py
+++ task_aggregator/aggregator/services/Task.py
@@ -1,9 +1,11 @@
 from datetime import datetime
+from typing import List
 
 
 class Task:
-    def __init__(self, service: str, link: str, description: str, labels: str,
-                 priority: str, deadline: datetime, difficulty: str):
+    def __init__(self, service: str, link: str, description: str,
+                 labels: List[str], priority: str, deadline: datetime,
+                 difficulty: str):
         self.service = service
         self.link = link
         self.description = description
