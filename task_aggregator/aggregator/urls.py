b54286fcd556c01c0e11ec0dca600687b3b872d9
Orianti <51492458+Orianti@users.noreply.github.com>
2021-04-16 18:26:30 +0300

    [#11] Add dashboard layout

--- task_aggregator/aggregator/urls.py
+++ task_aggregator/aggregator/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
 from aggregator.views.dashboard import DashboardView
+from aggregator.views.settings import SettingsView
 
 urlpatterns = [
     path('dashboard/', DashboardView.as_view(), name='dashboard'),
-    path('settings/', DashboardView.as_view(), name='settings'),
+    path('settings/', SettingsView.as_view(), name='settings'),
 ]
