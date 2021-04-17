8a9593599454306be2da0a38f94a00119998207f
Orianti <51492458+Orianti@users.noreply.github.com>
2021-04-17 22:58:30 +0300

    [#11] Fix linter error

--- task_aggregator/aggregator/views/register.py
+++ task_aggregator/aggregator/views/register.py
@@ -1,7 +1,6 @@
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import login
 from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import User
-from django.http import HttpResponseRedirect
 from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
