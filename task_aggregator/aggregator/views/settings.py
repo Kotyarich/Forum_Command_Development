316fc23a42bd8826e87c26ae0762de9a0043bbd3
Kotyarich <ndkotov@gmail.com>
2021-04-25 20:51:06 +0300

    [#20] Add settings view

--- task_aggregator/aggregator/views/settings.py
+++ task_aggregator/aggregator/views/settings.py
@@ -1,5 +1,19 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
+from aggregator.models import Token, Map
 
 
-class SettingsView(TemplateView):
+class SettingsView(LoginRequiredMixin, TemplateView):
     template_name = 'settings.html'
+    login_url = 'login'
+    redirect_field_name = 'settings'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        user = self.request.user
+        tokens = Token.objects.filter(user=user)
+        context['tokens'] = tokens
+        context['map'] = tokens[0].map
+
+        return context
